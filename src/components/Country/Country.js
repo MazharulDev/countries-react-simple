c8f9badeef74491df63570cdbfd6936e44afac71
Mazharul islam <miforbd@gmail.com>
2022-03-25 15:41:35 +0600

    country compunent style added

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Country.css'
 const Country = (props) => {
-    console.log(props.country);
-    const {name,flags}=props.country;
+    const {name,flags,capital,population}=props.country;
     return (
         <div className='country'>
             <img src={flags.png} alt="" />
             <h2>{name.common}</h2>
+            <p>Capital: {capital}</p>
+            <h5>Population: {population}</h5>
+            <button>Select Country</button>
         </div>
     );
 };
